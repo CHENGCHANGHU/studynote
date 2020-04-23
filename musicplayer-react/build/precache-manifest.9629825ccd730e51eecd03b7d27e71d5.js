b7156771edf9005106f066253450d5f23784c692
CHENGCHANGHU <1669675499@qq.com>
2020-04-23 20:32:11 +0800

    react music

---
+++ musicplayer-react/build/precache-manifest.9629825ccd730e51eecd03b7d27e71d5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0eef271c6834e292a09b9885e86d510",
+    "url": "/studynote/musicplayer-react/build/index.html"
+  },
+  {
+    "revision": "89db2561bf03303282bb",
+    "url": "/studynote/musicplayer-react/build/static/css/main.622a2614.chunk.css"
+  },
+  {
+    "revision": "5374a83a7f046ba7cbb8",
+    "url": "/studynote/musicplayer-react/build/static/js/2.bae2f5da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/studynote/musicplayer-react/build/static/js/2.bae2f5da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89db2561bf03303282bb",
+    "url": "/studynote/musicplayer-react/build/static/js/main.9ff695dc.chunk.js"
+  },
+  {
+    "revision": "4aec511a99a9ce3f02a6",
+    "url": "/studynote/musicplayer-react/build/static/js/runtime-main.9ad5fefa.js"
+  },
+  {
+    "revision": "1a12fbd6e8da2ab1a8b524e1c8588bb5",
+    "url": "/studynote/musicplayer-react/build/static/media/another-year.1a12fbd6.jpg"
+  },
+  {
+    "revision": "b283adba8e048d74eee91805f7ac9b89",
+    "url": "/studynote/musicplayer-react/build/static/media/another-year.b283adba.mp3"
+  },
+  {
+    "revision": "24ba70c3910d04272131a573168e57c1",
+    "url": "/studynote/musicplayer-react/build/static/media/drift-north.24ba70c3.mp3"
+  },
+  {
+    "revision": "81bdc6ee94a6bf119911092a4842a11c",
+    "url": "/studynote/musicplayer-react/build/static/media/drift-north.81bdc6ee.jpg"
+  },
+  {
+    "revision": "3d6ddd115d478f798d647af835fdb768",
+    "url": "/studynote/musicplayer-react/build/static/media/skin.3d6ddd11.jpg"
+  },
+  {
+    "revision": "9da8c78e4534e2fb2aa29a4b79d41a8d",
+    "url": "/studynote/musicplayer-react/build/static/media/skin.9da8c78e.mp3"
+  },
+  {
+    "revision": "94c8ebf54dc60d43b47b378e8b2e79fe",
+    "url": "/studynote/musicplayer-react/build/static/media/sunset-road.94c8ebf5.jpg"
+  },
+  {
+    "revision": "cc52929d1ddaa03f79cf42f9a71c81af",
+    "url": "/studynote/musicplayer-react/build/static/media/sunset-road.cc52929d.mp3"
+  }
+]);
