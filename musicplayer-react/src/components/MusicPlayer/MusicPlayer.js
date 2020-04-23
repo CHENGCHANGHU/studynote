76ada1701abb4076b9b9fea15a8bf68511703bb5
CHENGCHANGHU <1669675499@qq.com>
2020-04-23 20:09:45 +0800

    react music

--- musicplayer-react/src/components/MusicPlayer/MusicPlayer.js
+++ musicplayer-react/src/components/MusicPlayer/MusicPlayer.js
@@ -9,6 +9,9 @@ export default class MusicPlayer extends React.Component {
     this.state = {
       theMusicAudio,
       panelFlag: false,
+      controllerFlag: false,
+      controller: null,
+      localFileInput,
       musicList: [
         { id: 1, title: "日落大道", picSrc: require("./sample/img/sunset-road.jpg"), musicSrc: require("./sample/music/sunset-road.mp3") },
         { id: 2, title: "飘向北方", picSrc: require("./sample/img/drift-north.jpg"), musicSrc: require("./sample/music/drift-north.mp3") },
@@ -18,8 +21,8 @@ export default class MusicPlayer extends React.Component {
 
       // 当前播放音乐的状态
       currMusicObj: null,
-      currMusicIndex: -1,
-      currMusicDuration: 0,
+      currMusicIndex: 3,
+      currMusicDuration: 0,// 当前音乐时间总长
       currMusicTime: 0,
       playedBarWidth: 0,
       controllerLeft: 0,
@@ -54,6 +57,8 @@ export default class MusicPlayer extends React.Component {
     this.state.theMusicAudio.addEventListener("ended", this.endedHandler);
     this.state.theMusicAudio.addEventListener("timeupdate", this.timeUpdateHandler);
 
+    // document.addEventListener("mousemove", this.mouseMoveHandler);
+    document.addEventListener("mouseup", this.mouseUpHandler);
   }
 
   render() {
@@ -63,33 +68,54 @@ export default class MusicPlayer extends React.Component {
       playedBarWidth,
       controllerLeft,
       currMusicDuration,
+      currMusicIndex
     } = this.state;
     return (
       <div className="MusicPlayer" onKeyPress={this.keyPressHandler}>
         <div className="ControlBox">
           {/* <audio src="./sunset-road.mp3" id="TheMusic"></audio> */}
           <div className="prev">
-            <div id="prev"></div>
+            <div id="prev" onClick={this.changeMusicByPrev}></div>
           </div>
-          <div className="record-cover" id="recordCover">
+          <div className={`record-cover ${playFlag ? "pause-status" : "play-status"}`} id="recordCover">
             <img src={currMusicObj.picSrc} alt="封面" />
-            <div className="pause" id="pause"></div>
-            <div className="play" id="play"></div>
+            <div className="pause" id="pause" onClick={this.pauseMusic}></div>
+            <div className="play" id="play" onClick={this.playMusic}></div>
             {/* <div className="loading" id="loading">
               <div className="wave"></div>
             </div> */}
           </div>
           <div className="next">
-            <div id="next"></div>
+            <div id="next" onClick={this.changeMusicByNext}></div>
           </div>
           <div className="progress-box" id="progressBox">
             <div className="song-title">
               <span id="songTitle">{currMusicObj.title}</span>
-              <span className="more" id="more" onClick={this.openMusicPanel}></span>
+              <span className={`more ${panelFlag ? "more-active" : ""}`} id="more" onClick={this.openMusicPanel}></span>
             </div>
-            <div className="progress-bar" id="progressBar">
-              <div className="played-bar" style={{ width: playedBarWidth }} id="playedBar"></div>
-              <div className="controller" style={{ left: controllerLeft }} id="controller"></div>
+            <div className="progress-bar"
+              id="progressBar"
+              onClick={event => this.clickProgressBarHandler(event)}>
+              <div className="played-bar"
+                onClick={event => {
+                  console.log("played-bar");
+                  this.clickPlayedBarHandler(event);
+                  event.stopPropagation();
+                }}
+                style={{ width: playedBarWidth }} id="playedBar"></div>
+              <div className="controller"
+                onClick={event => {
+                  console.log("click controller");
+                  event.stopPropagation();
+                }}
+                onMouseDown={event => {
+                  this.mouseDownControllerHandler(event);
+                  event.preventDefault();
+                }}
+                // onMouseUp={event => {
+                //   this.mouseUpControllerHandler(event);
+                // }}
+                style={{ left: controllerLeft }} id="controller"></div>
             </div>
             <div className="song-duration">
               <span className="curr-duration">
@@ -104,17 +130,19 @@ export default class MusicPlayer extends React.Component {
           </div>
           <div className={`music-panel ${panelFlag ? "active-music-panel" : ""}`} id="musicPanel">
             <div className="panel-header">歌曲面板
-              <input type="file" className="local-file" id="localFile" />
+              <input type="file" className="local-file" ref={this.state.localFileInput} id="localFile" />
               <div className="local-music" id="localMusic">本地音乐</div>
             </div>
             <ul className="music-list">
-              {/* <li className="active-music"><a href="#">
-                <img className="pic" src={require("./sample/img/sunset-road.jpg")} />日落大道
-            </a></li> */}
               {
                 musicList.map((musicObj, index) => (
-                  <li key={musicObj.id}>
-                    <a href="#" onClick={() => this.changeMusicByMusicPanel(musicObj, index)}>
+                  <li key={musicObj.id}
+                    className={currMusicIndex === index ? "active-music" : ""}>
+                    <a href="#"
+                      onClick={event => {
+                        this.changeMusicByMusicPanel(musicObj, index);
+                        event.preventDefault();
+                      }}>
                       <img className="pic" src={musicObj.picSrc} />
                       {musicObj.title}
                     </a>
@@ -128,22 +156,25 @@ export default class MusicPlayer extends React.Component {
     );
   }
 
+  // 播放音乐
   playMusic = () => {
     console.log("MusicPlayer: 播放歌曲");
     this.setState({ ...this.state, playFlag: true });
     this.state.theMusicAudio.play();
   }
 
+  // 暂停音乐
   pauseMusic = () => {
     console.log("MusicPlayer: 暂停歌曲");
     this.setState({ ...this.state, playFlag: false });
     this.state.theMusicAudio.pause();
   }
 
-  changeMusicByMusicPanel = (musicObj, index) => {
+  // 歌曲列表项点击事件处理器
+  changeMusicByMusicPanel = async (musicObj, index) => {
     console.log(index, musicObj.id, musicObj.title);
     this.state.theMusicAudio.src = musicObj.musicSrc;
-    this.playMusic();
+    await this.playMusic();
     this.setState({
       ...this.state,
       currMusicObj: musicObj,
@@ -151,6 +182,88 @@ export default class MusicPlayer extends React.Component {
     });
   }
 
+  // 上一首点击事件处理器
+  changeMusicByPrev = async () => {
+    const { theMusicAudio, currMusicIndex, musicList } = this.state;
+    let index = currMusicIndex > 0 ? currMusicIndex - 1 : musicList.length - 1;
+    theMusicAudio.src = musicList[index].musicSrc;
+    await this.setState({
+      ...this.state,
+      currMusicObj: musicList[index],
+      currMusicIndex: index,
+    });
+    this.playMusic();
+  }
+
+  // 下一首点击事件处理器
+  changeMusicByNext = async () => {
+    const { theMusicAudio, currMusicIndex, musicList } = this.state;
+    let index = currMusicIndex < musicList.length - 1 ? currMusicIndex + 1 : 0;
+    theMusicAudio.src = musicList[index].musicSrc;
+    await this.setState({
+      ...this.state,
+      currMusicObj: musicList[index],
+      currMusicIndex: index,
+    });
+    this.playMusic();
+  }
+
+  // 进度条点击事件处理器
+  clickProgressBarHandler = event => {
+    const { theMusicAudio, currMusicDuration } = this.state;
+    const { clientX, target: { offsetLeft, offsetWidth } } = event;
+    const scale = Math.min(1, Math.max(0, (clientX - offsetLeft) / offsetWidth));
+    console.log("进度条点击事件", clientX, offsetLeft, offsetWidth, scale);
+    theMusicAudio.currentTime = scale * currMusicDuration;
+    this.setState({ ...this.state, currMusicTime: scale * currMusicDuration });
+  }
+
+  // 已播放进度条点击事件处理器
+  clickPlayedBarHandler = event => {
+    const { theMusicAudio, currMusicDuration } = this.state;
+    const { clientX, target: { parentNode: { offsetLeft, offsetWidth } } } = event;
+    const scale = Math.min(1, Math.max(0, (clientX - offsetLeft) / offsetWidth));
+    console.log("已播放进度条点击事件", clientX, offsetLeft, offsetWidth, scale);
+    theMusicAudio.currentTime = scale * currMusicDuration;
+    this.setState({ ...this.state, currMusicTime: scale * currMusicDuration });
+  }
+
+  // 进度控制器按下事件处理器
+  mouseDownControllerHandler = async event => {
+    console.log("进度控制器按下事件", event.clientX, event.target.parentNode.offsetLeft);
+    await this.setState({ ...this.state, controllerFlag: true, controller: event.target });
+    document.addEventListener("mousemove", this.mouseMoveHandler);
+    console.log(this.state.controllerFlag);
+    this.pauseMusic();
+  }
+
+  // 鼠标松开事件处理器
+  mouseUpHandler = async event => {
+    if (this.state.controllerFlag && this.state.controller) {
+      console.log("鼠标松开事件", event);
+      await this.setState({ ...this.state, controllerFlag: false, controller: null });
+      document.removeEventListener("mousemove", this.mouseMoveHandler);
+      this.playMusic();
+    }
+    event.stopPropagation();
+  }
+
+  // 鼠标移动事件
+  mouseMoveHandler = event => {
+    this.throttle(() => {
+      if (this.state.controllerFlag) {
+        const { theMusicAudio,
+          controller: { parentNode: { offsetLeft, offsetWidth } },
+          currMusicDuration } = this.state,
+          { clientX } = event,
+          scale = Math.min(1, Math.max(0, (clientX - offsetLeft) / offsetWidth));
+        theMusicAudio.currentTime = scale * currMusicDuration;
+        this.setState({ ...this.state, currMusicTime: scale * currMusicDuration });
+      }
+    }, 200)();
+  }
+
+  // 键盘按键处理器
   keyPressHandler = event => {
     console.log(event.keyCode);
     let { theMusicAudio, playFlag } = this.state;
@@ -192,5 +305,19 @@ export default class MusicPlayer extends React.Component {
     const min = Math.floor(sec / 60);
     sec = Math.floor(sec % 60);
     return `${min < 10 ? '0' + min : min}:${sec < 10 ? '0' + sec : sec}`;
+  }
+
+  throttle = (fn, delay) => {
+    let valid = true;
+    return function () {
+      if (!valid) {
+        return false;
+      }
+      valid = false;
+      setTimeout(() => {
+        fn();
+        valid = true;
+      }, delay);
+    }
   }
 }
